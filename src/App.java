34767dd42d56b0223d8c863faa4610f0dce0b2fa
augustocsmiranda <augustocezarmiranda@gmail.com>
2021-06-23 11:19:45 -0300

    Teste 2

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Teste");
+        System.out.println("Teste 2");
     }
 }
