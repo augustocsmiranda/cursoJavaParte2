ff6030a938a84bd623b1b0a582836a924a69931e
augustocsmiranda <augustocezarmiranda@gmail.com>
2021-07-06 18:29:01 -0300

    Aula 133

---
+++ src/aula133/entities/Rectangle.java
@@ -0,0 +1,47 @@
+package aula133.entities;
+
+import aula133.entities.enums.Color;
+
+public class Rectangle extends Shape{
+
+    private Double width;
+    private Double height;
+
+    public Rectangle(){
+        super();
+    }
+
+    public Rectangle(Color color, Double width, Double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+  /*  public Rectangle(Double width, Double height) {
+        this.width = width;
+        this.height = height;
+    }*/
+
+public Double getWidth() {
+    return width;
+}
+
+public void setWidth(Double width) {
+    this.width = width;
+}
+
+public Double getHeight() {
+    return height;
+}
+
+public void setHeight(Double height) {
+    this.height = height;
+}
+
+    @Override
+    public double area() {
+        // TODO Auto-generated method stub
+        return width * height; 
+    
+    }
+}
---
+++ src/aula133/entities/Shape.java
@@ -0,0 +1,26 @@
+package aula133.entities;
+
+import aula133.entities.enums.Color;
+
+public abstract class Shape {
+    
+    private Color color;
+
+    public Shape(){
+
+    }
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+    
+    public abstract double area();
+}
